5babf223250aff6fc2c736ff85691ccfbafd4790
BogdanBida <bogdan_bida@mail.ua>
2021-03-05 21:25:00 +0200

    Fixed translator initializing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
+import { LOCATION_INITIALIZED } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+
+export function appInitializerFactory(translate: TranslateService, injector: Injector): () => Promise<any> {
+  return () => new Promise<any>((resolve: any) => {
+    const locationInitialized = injector.get(LOCATION_INITIALIZED, Promise.resolve(null));
+    locationInitialized.then(() => {
+      const langToSet = environment.defaultLang;
+      translate.setDefaultLang(environment.defaultLang);
+      translate.use(langToSet).subscribe(() => {
+        console.info(`Successfully initialized '${langToSet}' language.'`);
+      }, err => {
+        console.error(`Problem with '${langToSet}' language initialization.'`);
+      }, () => {
+        resolve(null);
+      });
+    });
+  });
+}
 
 
 @NgModule({
@@ -25,16 +43,19 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     NoopAnimationsModule,
     TranslateModule.forRoot({
-      defaultLanguage: environment.defaultLang,
       loader: {
         provide: TranslateLoader,
         useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
         deps: [HttpClient],
       },
     }),
-
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: appInitializerFactory,
+    deps: [TranslateService, Injector],
+    multi: true
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
