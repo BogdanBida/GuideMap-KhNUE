f33dc87786d44c61309f96761951daca750a4e0a
BogdanBida <bogdan_bida@mail.ua>
2021-03-06 13:21:34 +0200

    Removed unnecessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>',
+  styles: ['']
 })
 export class AppComponent {
   title = 'guidemap';
