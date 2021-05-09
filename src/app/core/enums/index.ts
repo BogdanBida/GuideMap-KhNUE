6f11e602bd6c5601419aeea4b4c7d0bd5eb357df
Yegor <kotelevets1yehor@hneu.net>
2021-05-09 03:32:52 +0300

    Refactor

--- src/app/core/enums/index.ts
+++ src/app/core/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './guide-map-feature-point-category.enum';
 export * from './json-files.enum';
 export * from './lang.enum';
+export * from './map-stairs-floor-switcher.enum';
--- src/app/core/services/map-dot.service.ts
+++ src/app/core/services/map-dot.service.ts
@@ -1,13 +1,14 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
 import { Injectable } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Circle, Use } from '@svgdotjs/svg.js';
-import { GuideMapFeaturePointCategory } from '../enums';
+import { GuideMapFeaturePointCategory, MapStairsFloorSwitcher } from '../enums';
 import { GuideMapRoomProperties } from '../models';
 import { MapPointUtils } from '../utils';
 import { FloorService } from './floor.service';
 import { MapPathService } from './map-path.service';
 import { MapService } from './map.service';
 
+@UntilDestroy()
 @Injectable({
   providedIn: 'root',
 })
@@ -29,7 +30,7 @@ export class MapDotService {
   }
 
   public drawPoint(
-    Dot: Circle,
+    dot: Circle,
     location: GuideMapRoomProperties,
     isEndpoint = true
   ): Circle {
@@ -38,44 +39,49 @@ export class MapDotService {
       isEndpoint
     );
 
-    if (Dot) {
-      Dot.remove();
+    if (dot) {
+      dot.remove();
     }
 
     if (this._mapService.svgInstance && location) {
       const radius = 25;
       const maxRadius = 500;
+      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
       const maxRadiusHalf = maxRadius / 2;
+      const dotSizeMaxAnimationRadius = 20;
+      const dotSize = radius + dotSizeMaxAnimationRadius;
       const { x, y } = location;
 
-      Dot = this._mapService.svgInstance
+      dot = this._mapService.svgInstance
         .circle(maxRadius)
         .attr({ fill: pointColor, opacity: 0 })
         .move(x - maxRadiusHalf, y - maxRadiusHalf);
 
-      Dot.animate({ duration: 2500 })
+      dot
+        .animate({ duration: 2500 })
         .size(radius, radius)
         .attr({ fill: pointColor, opacity: 0.75 });
 
-      Dot.animate({ ease: '<' });
+      dot.animate({ ease: '<' });
 
-      Dot.animate({ duration: 1000, ease: '<>' })
+      dot
+        .animate({ duration: 1000, ease: '<>' })
         .loop(Infinity, true)
-        .size(radius + 20, radius + 20)
+        .size(dotSize, dotSize)
         .attr({ opacity: 0.4 });
     }
 
-    return Dot;
+    return dot;
   }
 
   private _subscribeOnPathCoordinatesChanges(): void {
-    this._mapPathService.pathCoordinatesChanges$.subscribe(
-      ([userLocation, endPoint]) => {
+    this._mapPathService.pathCoordinatesChanges$
+      .pipe(untilDestroyed(this))
+      .subscribe(([userLocation, endPoint]) => {
         this.endpointDot = this.drawPoint(this.endpointDot, endPoint);
         this.userDot = this.drawPoint(this.userDot, userLocation, false);
         this._drawFloorSwitcher([userLocation, endPoint]);
-      }
-    );
+      });
   }
 
   private _drawFloorSwitcher(points: GuideMapRoomProperties[]): void {
@@ -85,47 +91,59 @@ export class MapDotService {
 
     this.stairsFloorSwitcher?.remove();
 
-    if (stairsPointIndex === 0) {
+    const isShowDownArrow = stairsPointIndex === 0;
+    const isShowArrowUp = stairsPointIndex > -1;
+
+    if (isShowDownArrow) {
       this.stairsFloorSwitcher = this._drawArrow(
         points[stairsPointIndex],
-        'arrow-down-d'
+        MapStairsFloorSwitcher.ArrowDown
       );
 
       return;
     }
 
-    if (stairsPointIndex > -1) {
+    if (isShowArrowUp) {
       this.stairsFloorSwitcher = this._drawArrow(
         points[stairsPointIndex],
-        'arrow-up'
+        MapStairsFloorSwitcher.ArrowUp
       );
     }
   }
 
   private _drawArrow(
     stairsPoint: GuideMapRoomProperties,
-    arrowDirection = 'arrow-up'
+    arrowDirection: MapStairsFloorSwitcher
   ): Use {
+    const xOffset = 25;
+    const yOffset = 80;
+    const arrowsOverStairsPointCoordinates = {
+      x: stairsPoint.x - xOffset,
+      y: stairsPoint.y - yOffset,
+    };
+    const arrowsSize = 50;
+    const arrowsStyles = `
+      filter: drop-shadow(0 2px 1px rgba(0, 0, 0, 0.5))
+              drop-shadow(1px 3px 2px rgba(0, 0, 0, 0.4))
+              drop-shadow(0 6px 12px rgba(0, 0, 0, 0.3));
+      pointer-events: all;
+      cursor: pointer;
+    `;
+
     return this._mapService.svgInstance
       .use(arrowDirection, 'assets/icons/sprite.svg')
-      .attr(
-        'style',
-        `
-        filter: drop-shadow(0 2px 1px rgba(0, 0, 0, 0.5))
-                drop-shadow(1px 3px 2px rgba(0, 0, 0, 0.4))
-                drop-shadow(0 6px 12px rgba(0, 0, 0, 0.3));
-        pointer-events: all;
-        cursor: pointer;
-    `
-      )
-      .size(50, 50)
+      .attr('style', arrowsStyles)
+      .size(arrowsSize, arrowsSize)
       .id(arrowDirection)
-      .move(stairsPoint.x - 25, stairsPoint.y - 80)
+      .move(
+        arrowsOverStairsPointCoordinates.x,
+        arrowsOverStairsPointCoordinates.y
+      )
       .click(() => this._onFloorSwitcherClick(arrowDirection));
   }
 
-  private _onFloorSwitcherClick(arrowDirection: string): void {
-    if (arrowDirection === 'arrow-up') {
+  private _onFloorSwitcherClick(arrowDirection: MapStairsFloorSwitcher): void {
+    if (arrowDirection === MapStairsFloorSwitcher.ArrowUp) {
       const finalEndpoint = this._mapPathService.finalEndpoint$.getValue();
 
       this._floorService.floor = finalEndpoint.floor;
