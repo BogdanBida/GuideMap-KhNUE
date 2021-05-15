a37cfdaf470978ca1a1104592424065ec87e3144
BogdanBida <bogdan_bida@mail.ua>
2021-05-16 01:41:00 +0300

    Moved info button to separate component

--- src/app/feature/map/map.component.ts
+++ src/app/feature/map/map.component.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
-import { InfoDialogComponent } from './dialogs/info-dialog/info-dialog.component';
 
 @Component({
   selector: 'app-map',
@@ -10,18 +8,5 @@ import { InfoDialogComponent } from './dialogs/info-dialog/info-dialog.component
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
-  constructor(public dialog: MatDialog) {}
-
-  public x: number;
-
-  public y: number;
-
   public zoomFactor: number = environment.defaultZoomFactor;
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(InfoDialogComponent);
-
-    dialogRef.addPanelClass('fullscreen-modal');
-    dialogRef.afterClosed().subscribe();
-  }
 }
