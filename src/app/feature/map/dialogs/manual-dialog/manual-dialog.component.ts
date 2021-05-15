3e9aea041d3a7cc68064faae29bb8a4bd0296dac
BogdanBida <bogdan_bida@mail.ua>
2021-05-16 01:41:26 +0300

    Updated info dialog to manual dialog

---
+++ src/app/feature/map/dialogs/manual-dialog/manual-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-dialog',
+  templateUrl: './manual-dialog.component.html',
+  styleUrls: ['./manual-dialog.component.scss'],
+})
+export class ManualDialogComponent {}
