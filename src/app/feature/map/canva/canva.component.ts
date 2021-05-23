c409ce6873bf323c9260a3f9f7811b97f9a33244
BogdanBida <bogdan_bida@mail.ua>
2021-05-23 04:24:15 +0300

    Updated panzoom

--- src/app/feature/map/canva/canva.component.ts
+++ src/app/feature/map/canva/canva.component.ts
@@ -1,27 +1,15 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { PanZoomConfig } from 'ngx-panzoom';
 import { MapService } from 'src/app/core/services/map.service';
-import { DragNDrop } from 'src/app/core/utils';
 import {
   FloorService,
   MapDataProviderService,
   MapDotService,
   MapGraphService,
   MapPathService,
+  PanZoomService,
 } from './../../../core/services';
-import { MapZoomService } from './../../../core/services/map-zoom.service';
-
-const WIDTH = 3500;
-const HEIGHT = 2550;
-const DEFAULT_TRANSITION_SPEED = 500;
-const WHEEL_ZOOM_STEP = 0.05;
 
 @UntilDestroy()
 @Component({
@@ -36,21 +24,20 @@ export class CanvaComponent implements OnInit, AfterViewInit {
     private readonly _mapDotService: MapDotService,
     private readonly _mapDataProviderService: MapDataProviderService,
     private readonly _floorService: FloorService,
-    private readonly _renderer: Renderer2,
-    private readonly _mapZoomService: MapZoomService,
-    private readonly _mapService: MapService
-  ) {}
+    private readonly _mapService: MapService,
+    private readonly _panZoomService: PanZoomService
+  ) {
+    this.panzoomConfig = this._panZoomService.init();
+  }
 
-  @ViewChild('target') public readonly elementRef: ElementRef<HTMLElement>;
-
-  public dragNDrop = DragNDrop.onDrag(WIDTH, HEIGHT);
-
-  public readonly scaleTransform$ = this._mapZoomService.scaleTransform$;
+  public panzoomConfig: PanZoomConfig;
 
   public ngOnInit(): void {
     this._mapDataProviderService.init$().subscribe(() => {
       this._mapGraphService.createGraph();
     });
+
+    this._panZoomService.init();
   }
 
   public ngAfterViewInit(): void {
@@ -66,7 +53,7 @@ export class CanvaComponent implements OnInit, AfterViewInit {
         this._mapService.clearPath();
 
         if (point) {
-          this._moveMapTo(point?.x, point?.y);
+          this._panZoomService.centerTo(point?.x, point?.y);
         }
       });
 
@@ -76,67 +63,11 @@ export class CanvaComponent implements OnInit, AfterViewInit {
         this._mapService.clearPath();
 
         if (point) {
-          this._moveMapTo(point.x, point.y);
+          this._panZoomService.centerTo(point?.x, point?.y);
         }
       });
 
     this._mapDotService.init();
     this._mapService.init();
-  }
-
-  public zoomIn(): void {
-    this._mapZoomService.zoomIn(WHEEL_ZOOM_STEP);
-  }
-
-  public zoomOut(): void {
-    this._mapZoomService.zoomOut(WHEEL_ZOOM_STEP);
-  }
-
-  public resetZoom(): void {
-    this._mapZoomService.resetZoomFactor();
-  }
-
-  private _moveMapTo(
-    left: number,
-    top: number,
-    transitionSpeed: number = DEFAULT_TRANSITION_SPEED
-  ): void {
-    if (left && top) {
-      // adapt values ​​for zoom
-      const zoomFactor = this._mapZoomService.zoomFactor;
-
-      top = top * zoomFactor + (HEIGHT - HEIGHT * zoomFactor) / 2;
-      left = left * zoomFactor + (WIDTH - WIDTH * zoomFactor) / 2;
-      // centering
-      let targetTop = top - window.innerHeight / 2;
-      let targetLeft = left - window.innerWidth / 2;
-
-      targetTop = targetTop < 0 ? 0 : targetTop;
-      targetLeft = targetLeft < 0 ? 0 : targetLeft;
-
-      this._renderer.setStyle(
-        this.elementRef.nativeElement,
-        'transition',
-        `${transitionSpeed}ms`
-      );
-      this._renderer.setStyle(
-        this.elementRef.nativeElement,
-        'top',
-        -targetTop + 'px'
-      );
-      this._renderer.setStyle(
-        this.elementRef.nativeElement,
-        'left',
-        -targetLeft + 'px'
-      );
-
-      setTimeout(() => {
-        this._renderer.setStyle(
-          this.elementRef.nativeElement,
-          'transition',
-          'inherit'
-        );
-      }, transitionSpeed);
-    }
   }
 }
