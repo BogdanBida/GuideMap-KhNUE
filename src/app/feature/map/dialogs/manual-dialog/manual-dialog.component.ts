524eb4f527c9f2e3a2b72baf0c2a414797235069
paleodon <paleodon53@gmail.com>
2021-05-24 19:02:51 +0300

    update manual modal

--- src/app/feature/map/dialogs/manual-dialog/manual-dialog.component.ts
+++ src/app/feature/map/dialogs/manual-dialog/manual-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ManualTitleTranslateKeys } from 'src/app/core/enums/manual-titles.translate-keys.enum.ts.enum';
+import { ManualTranslateKeys } from 'src/app/core/enums/manual.translate-keys.enum';
 
 @Component({
   selector: 'app-manual-dialog',
@@ -7,13 +7,54 @@ import { ManualTitleTranslateKeys } from 'src/app/core/enums/manual-titles.trans
   styleUrls: ['./manual-dialog.component.scss'],
 })
 export class ManualDialogComponent {
-  public readonly mainTitle = ManualTitleTranslateKeys.Manual;
 
-  public readonly interfaceTitle = ManualTitleTranslateKeys.Interface;
+  public readonly mainTitle = ManualTranslateKeys.Title;
 
-  public readonly authorsTitle = ManualTitleTranslateKeys.Authors;
+  public readonly mainDescription = ManualTranslateKeys.Description;
 
-  public readonly supportTitle = ManualTitleTranslateKeys.Support;
+  public readonly interfaceTitle = ManualTranslateKeys.Interface;
 
-  public readonly footerTitle = ManualTitleTranslateKeys.Footer;
+  public readonly authorsTitle = ManualTranslateKeys.Authors;
+
+  public readonly supportTitle = ManualTranslateKeys.Support;
+
+  public readonly footerTitle = ManualTranslateKeys.Footer;
+
+  public readonly interfaceItems = [
+    {
+      tittle: ManualTranslateKeys.MainMap,
+      url: 'assets/guide/main-map.png',
+      description: ManualTranslateKeys.MainMapDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.StartPoint,
+      url: 'assets/guide/start-point.png',
+      description: ManualTranslateKeys.StartPointDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.FinishPoint,
+      url: 'assets/guide/finish-point.png',
+      description: ManualTranslateKeys.FinishPointDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.FloorSwitch,
+      url: 'assets/guide/floor-switch.png',
+      description: ManualTranslateKeys.FloorSwitchDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.ZoomEdit,
+      url: 'assets/guide/zoom-edit.png',
+      description: ManualTranslateKeys.ZoomEditDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.Settings,
+      url: 'assets/guide/settings.png',
+      description: ManualTranslateKeys.SettingsDescription,
+    },
+    {
+      tittle: ManualTranslateKeys.Manual,
+      url: 'assets/guide/manual.png',
+      description: ManualTranslateKeys.ManualDescription,
+    }
+  ];
 }
